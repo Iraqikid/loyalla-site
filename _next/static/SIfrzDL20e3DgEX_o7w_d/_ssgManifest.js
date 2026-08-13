c0178438a8390a58f5637351df0a29f9e0dfb518
Abdulla Taha <abdulla.taha@hotmail.com>
2026-08-13 16:14:15 +0400

    Loyalla public site — static build

---
+++ _next/static/SIfrzDL20e3DgEX_o7w_d/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fprivacy","\u002F[locale]\u002Fterms","\u002F[locale]\u002Fvenues","\u002F[locale]\u002Fvenues\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
